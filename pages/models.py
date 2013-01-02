e6a0f6d8b2b696d2bc23aef6fb261875330ade89
Kilgharrah <19noname19@gmail.com>
2013-01-02 15:37:36 +0100

    add page model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from menu.models import Menu
+
+from transmeta import TransMeta
+
+class Page(models.Model):
+    __metaclass__ = TransMeta
+
+    menu = models.ForeignKey(Menu)
+    title = models.CharField(max_length=30, blank=True, verbose_name=u'Title')
+    description = models.TextField(verbose_name=u'Content')
+
+    class Meta:
+        translate = ('title', 'description', )
+
+    def __unicode__(self):
+        return self.title
