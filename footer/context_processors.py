c4bb9bcdc662b90fb88a734f1f88a47ca01a4152
kilgharrah <19noname19@gmail.com>
2013-01-10 12:23:27 +0100

    add order, title, link field in footer logos

--- footer/context_processors.py
+++ footer/context_processors.py
@@ -2,11 +2,14 @@ from models import Footer_Info, Footer_Logos
 from django.conf import settings
 
 def footer_text(request):
+    footer_text = Footer_Info.objects.all()
+    #footer_text = footer_texts.reverse()[0] 
+    
     return {
-        'footer_text': Footer_Info.objects.all(),
+        'footer_text': footer_text,
     }
 
 def footer_logos(request):
     return {
-        'footer_logos': Footer_Logos.objects.all(),
+        'footer_logos': Footer_Logos.objects.all().order_by('order'),
     }
