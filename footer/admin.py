b87f5b6e7e4ec239ce826544bf0adef49aa53405
kilgharrah <19noname19@gmail.com>
2013-01-10 10:47:10 +0100

    show logo name in footer logos admin

--- footer/admin.py
+++ footer/admin.py
@@ -7,7 +7,19 @@ class Footer_InfoAdmin(admin.ModelAdmin):
     ordering = ()
 
 class Footer_LogosAdmin(admin.ModelAdmin):
-    list_display = ( ('logo'), )
+    list_display = ( ('logo_name'), )
+
+    def logo_name(self, obj):
+        try:
+            if obj.logo:
+                return u"%s" % ( obj.logo.__unicode__().split("/")[-1])
+            else:
+                return ""
+        except IOError:
+            return ""
+
+    logo_name.allow_tags = True
+    logo_name.short_description = u"Logo Name"
 
 admin.site.register(Footer_Info, Footer_InfoAdmin )
 admin.site.register(Footer_Logos, Footer_LogosAdmin)
