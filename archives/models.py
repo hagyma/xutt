af9e9b37b7834fa22d138fc1232eace593895f6f
beno <19noname19@gmail.com>
2013-01-29 10:04:12 +0100

    #fix #arhives model -> transmeta

--- archives/models.py
+++ archives/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
+from transmeta import TransMeta
+
 class Project(models.Model):
+	__metaclass__ = TransMeta
+
 	title = models.CharField(max_length=64, verbose_name=u'Title')
 	description = models.TextField(verbose_name=u'Description')
 	image = models.ImageField(upload_to='uploads/project/', blank=True, null= True, verbose_name=u'Image')
 
 	created_at = models.DateTimeField(auto_now_add=True, editable=False)
 	updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+	class Meta:
+		translate = ('title', 'description', 'image')
 
 	class Media:
 		js = [
@@ -18,12 +25,17 @@ class Project(models.Model):
 		return u'%s' % self.title
 
 class Book(models.Model):
+	__metaclass__ = TransMeta
+
 	title = models.CharField(max_length=64, verbose_name=u'Title')
 	description = models.TextField(verbose_name=u'Description')
 	image = models.ImageField(upload_to='uploads/book/', blank=True, null= True, verbose_name=u'Image')
 
 	created_at = models.DateTimeField(auto_now_add=True, editable=False)
 	updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+	class Meta:
+		translate = ('title', 'description', 'image')
 
 	class Media:
 		js = [
@@ -35,12 +47,17 @@ class Book(models.Model):
 		return u'%s' % self.title
 
 class Publication(models.Model):
+	__metaclass__ = TransMeta
+
 	title = models.CharField(max_length=64, verbose_name=u'Title')
 	description = models.TextField(verbose_name=u'Description')
 	image = models.ImageField(upload_to='uploads/publication/', blank=True, null= True, verbose_name=u'Image')
 
 	created_at = models.DateTimeField(auto_now_add=True, editable=False)
 	updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+	class Meta:
+		translate = ('title', 'description', 'image' )
 
 	class Media:
 		js = [
